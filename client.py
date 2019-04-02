96598875fbc7ada4d7aab02f524862618de18592
Eugene Oca <eugenebasiliscooca@gmail.com>
2019-04-03 01:02:53 +0800

    Client Update

--- client.py
+++ client.py
@@ -7,9 +7,11 @@ class Client():
 
     name = ""
     host_address = ""
-    client_listener_sock = ""
+    
     port = 2000
     o_clients = []
+    server_sock = ""
+    client_sock = ""
 
     def __init__(self, name = "Client", port=2000):
         self.set_name(name)
@@ -17,13 +19,23 @@ class Client():
         self.host_address = self.get_host()
 
     def run(self):
-        
+        self.server_descriptor = self.detect_server(self.port)
         # --- Layout ---
         # Find server by sending REG signal (Registration)
         # Once received an ACK signal, store server socket then do handshake (Verification)
         # Monitor external and internal immediate change signals (Monitoring)
         # For internal changes, transmit broadcast signal through the server (Synchronization)
         pass
+
+    def detect_server(self, port):
+        self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client_sock.bind((self.host_address, self.port))
+        self.client_log("Client running at "+self.host_address+":"+str(self.port))
+        print("Looking for active server...")
+        
+
+
+        return 0
 
     def set_name(self, name):
         self.name = name
@@ -33,6 +45,9 @@ class Client():
 
     def get_host(self):
         return socket.gethostbyname(socket.gethostname())
+
+    def client_log(self, message):
+        print("["+self.name+"] "+message)
 
 if __name__ == "__main__":
     client = Client("Syncnet Client", 2000)
