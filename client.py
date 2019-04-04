97051fd0f0abea8ba87c39b0db165c715fc894a8
Eugene Oca <eugenebasiliscooca@gmail.com>
2019-04-04 22:22:29 +0800

    Fixed minor bugs

--- client.py
+++ client.py
@@ -18,9 +18,9 @@ class Client():
 
     def __init__(self, name = "Client", port = 2000):
 
+        self.server_port = port
         port = random.randint(port+1, 5000) # Set random port
         self.name = name
-        self.server_port
         self.client_port = port
         self.client_address = self.get_host()
 
@@ -35,25 +35,27 @@ class Client():
         self.client_log("Client running at "+self.client_address+":"+str(self.client_port))
 
         # --- Layout ---
-        # Find server by sending REG signal (Registration)
-        # Once received an ACK signal, store server socket then do handshake (Verification)
+        # Find server by sending REG signal (Registration) - DONE
+        # Once received an ACK signal, store server socket then do handshake (Verification) - DONE
         # Monitor external and internal immediate change signals (Monitoring)
         # For internal changes, transmit broadcast signal through the server (Synchronization)
         print("Looking for active server...")
-        while self.server_address=="":
-            self.server_address = self.detect_server(self.server_port)
-        pass
+        while self.server_sock=="":
+            self.server_sock = self.detect_server(self.server_port)
+        
+        self.server_address = self.server_sock[0]
+        print(self.server_sock)
 
     def detect_server(self, port):
         base_address = self.get_base_address()
-        for last_octet in range(1,255):
+        for last_octet in range(0,255):
             candidate_address = base_address+"."+str(last_octet)
             self.client_sock.sendto("REG".encode(), (candidate_address, port))
             try:
-                data, _ = self.client_sock.recvfrom(1024)
+                data, sck = self.client_sock.recvfrom(1024)
                 if data.decode("UTF-8")=="ACK":
-                    print("Registered successfully on server "+candidate_address+":"+str(port))
-                    return candidate_address
+                    print("Registered successfully on server "+sck[0]+":"+str(sck[1]))
+                    return sck
             except:
                 pass
         return "" # Return NULL String
